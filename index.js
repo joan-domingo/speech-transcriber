f0ef1a1ce8f7b6f18d47d10479237ac22a2c9e10
Joan Domingo <joands@gmail.com>
2018-06-13 10:24:16 +0200

    transcribe flac file

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 // Imports the Google Cloud client library
 const speech = require('@google-cloud/speech');
-const fs = require('fs');
 
 // Creates a client
 const client = new speech.SpeechClient();
 
-// The name of the audio file to transcribe
-const fileName = './audio.raw';
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+const gcsUri = 'gs://cloud-samples-tests/speech/brooklyn.flac';
+const encoding = 'FLAC';
+const sampleRateHertz = 16000;
+const languageCode = 'en-US';
 
-// Reads a local audio file and converts it to base64
-const file = fs.readFileSync(fileName);
-const audioBytes = file.toString('base64');
-
-// The audio file's encoding, sample rate in hertz, and BCP-47 language code
-const audio = {
-  content: audioBytes,
-};
 const config = {
-  encoding: 'LINEAR16',
-  sampleRateHertz: 16000,
-  languageCode: 'en-US',
-};
-const request = {
-  audio: audio,
-  config: config,
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  languageCode: languageCode,
 };
 
-// Detects speech in the audio file
+const audio = {
+  uri: gcsUri,
+};
+
+const request = {
+  config: config,
+  audio: audio,
+};
+
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
 client
-  .recognize(request)
+  .longRunningRecognize(request)
+  .then(data => {
+    const operation = data[0];
+    // Get a Promise representation of the final result of the job
+    return operation.promise();
+  })
   .then(data => {
     const response = data[0];
     const transcription = response.results
