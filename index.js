1adf3d01f2c34ff7bbd4f32407a7f1b6f3d1d9c5
Joan Domingo <joands@gmail.com>
2018-06-13 10:38:21 +0200

    set up eslint

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const sampleRateHertz = 16000;
 const languageCode = 'en-US';
 
 const config = {
-  encoding: encoding,
-  sampleRateHertz: sampleRateHertz,
-  languageCode: languageCode,
+  encoding,
+  sampleRateHertz,
+  languageCode,
 };
 
 const audio = {
@@ -23,26 +23,26 @@ const audio = {
 };
 
 const request = {
-  config: config,
-  audio: audio,
+  config,
+  audio,
 };
 
 // Detects speech in the audio file. This creates a recognition job that you
 // can wait for now, or get its result later.
 client
   .longRunningRecognize(request)
-  .then(data => {
+  .then((data) => {
     const operation = data[0];
     // Get a Promise representation of the final result of the job
     return operation.promise();
   })
-  .then(data => {
+  .then((data) => {
     const response = data[0];
     const transcription = response.results
       .map(result => result.alternatives[0].transcript)
       .join('\n');
     console.log(`Transcription: ${transcription}`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error('ERROR:', err);
   });
